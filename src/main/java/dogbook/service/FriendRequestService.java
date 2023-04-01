63894f582146b8b89fa22c3bda5e3dc2803c6c89
usdaproved <tbrock959@gmail.com>
2023-04-01 09:41:56 -0500

    An unused import which isn't found in the maven dependencies is breaking the build for me. Perhaps intellij just auto-imports this no problem?

--- src/main/java/dogbook/service/FriendRequestService.java
+++ src/main/java/dogbook/service/FriendRequestService.java
@@ -4,7 +4,6 @@ import dogbook.model.FriendRequest;
 import dogbook.model.Friendship;
 import dogbook.repository.FriendRequestRepo;
 import dogbook.repository.FriendshipRepo;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
